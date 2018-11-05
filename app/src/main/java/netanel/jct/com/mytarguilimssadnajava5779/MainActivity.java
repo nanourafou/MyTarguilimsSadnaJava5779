f789482f57d7e6fcc73eb3a973b0fe2784f5feac
QuickS0rt <ncohenso@g.jct.ac.il>
2018-11-05 18:37:16 +0200

    Hello WORLD

--- app/src/main/java/netanel/jct/com/mytarguilimssadnajava5779/MainActivity.java
+++ app/src/main/java/netanel/jct/com/mytarguilimssadnajava5779/MainActivity.java
@@ -1,16 +1,67 @@
 package netanel.jct.com.mytarguilimssadnajava5779;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MyFirstProgram";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final Context context = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(TAG,"onCreate");
+
+        Button btnT1 = (Button) findViewById(R.id.btn_t1);
+        btnT1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentI1 = new Intent(context,Targuil1.class);
+                startActivity(intentI1);
+            }
+        });
+
+        Button btnT2 = (Button) findViewById(R.id.btn_t2);
+        btnT2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentI1 = new Intent(context,Targuil2.class);
+                startActivity(intentI1);
+            }
+        });
+
+        Button btnT3 = (Button) findViewById(R.id.btn_t3);
+        btnT3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentI1 = new Intent(context,Targuil3.class);
+                startActivity(intentI1);
+            }
+        });
+
+        Button btnT4 = (Button) findViewById(R.id.btn_t4);
+        btnT4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentI1 = new Intent(context,Targuil4.class);
+                startActivity(intentI1);
+            }
+        });
+
+        Button btnT5 = (Button) findViewById(R.id.btn_t5);
+        btnT5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentI1 = new Intent(context,Targuil5.class);
+                startActivity(intentI1);
+            }
+        });
+
+
     }
 }
